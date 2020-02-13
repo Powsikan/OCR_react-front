e0d191f9f84f3658074ca63b33fb0aabd7ba86bd
powsokan <powsi1997@gmail.com>
2020-02-13 19:22:28 +0530

    pages components added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import {  BrowserRouter as Route, Router, Switch} from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Home from "./components/Home";
+import LogIn from "./components/LogIn";
+import SignUp from "./components/SignUp";
+import ViewResult from "./components/ViewResult";
+
 
 function App() {
-  return (
-    <div className="App">
-     <h1>hello</h1>
-    </div>
-  );
+    return (
+        <div>
+            <Router>
+                <div>
+                    <NavBar></NavBar>
+                    <hr/>
+                    <Switch>
+                        <Route exact path='/' component={Home}/>
+
+                        <Route path='/login' component={LogIn}/>
+
+                        <Route path='/signup' component={SignUp}/>
+                        <Route path='/viewresult' component={ViewResult}/>
+
+                    </Switch>
+                </div>
+            </Router>
+
+
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/ViewResult.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class ViewResult extends Component {
+    render() {
+        return (
+            <div>
+                view result
+            </div>
+        );
+    }
+}
+
+export default ViewResult;
