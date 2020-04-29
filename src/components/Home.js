1edfb690fe50b1fa3f38e1f64d8f7cec6d9f6053
powsokan <powsi1997@gmail.com>
2020-04-29 10:31:10 +0530

    file choose button added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React, {Component, useRef} from 'react';
+import React, {Component} from 'react';
 
 class Home extends Component {
     constructor(props) {
@@ -23,17 +23,18 @@ class Home extends Component {
                     <hr className="my-4"/>
                         <p>It detect the text in the Image and give back to you as text format.</p>
                     <div className="upload">
-                        <input type="file" ref={this.fileInput} />                    </div>
+                        <input type="file" ref={this.fileInput} style={{display:"none"}}/>
+                    </div>
                         <button className="btn btn-primary btn-lg"  onSubmit={this.handleSubmit}  >Upload Image</button>
                 </div>
 
                 <div className="card">
-                    <img src="../images/upl.png" className="card-img-top" alt="Uploaded image"/>
+                    <img src="../images/upl.png" className="card-img-top" alt="uploaded"/>
                         <div className="card-body">
                             <h5 className="card-title">Result for Uploaded Image</h5>
                             <p className="card-text">Some quick example text to build on the card title and make up the
                                 bulk of the card's content.</p>
-                            <a href="#" className="btn btn-success">Save the result</a>
+                            <button className="btn btn-success">Save the result</button>
                         </div>
                 </div>
 
--- src/components/ViewResult.js
+++ src/components/ViewResult.js
@@ -10,7 +10,7 @@ class ViewResult extends Component {
                         <h5 className="card-title">Result for Uploaded Image</h5>
                         <p className="card-text">Some quick example text to build on the card title and make up the
                             bulk of the card's content.</p>
-                        <a href="#" className="btn btn-danger">Delete the result</a>
+                        <button  className="btn btn-danger">Delete the result</button>
                     </div>
                 </div>
             </div>
