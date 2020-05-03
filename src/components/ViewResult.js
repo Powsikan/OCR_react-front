20081d2bc42f59a7e1fe8446630b43ac100daa22
powsokan <powsi1997@gmail.com>
2020-05-03 19:50:49 +0530

    view result api call

--- src/components/ViewResult.js
+++ src/components/ViewResult.js
@@ -1,32 +1,37 @@
-import React from 'react';
+import React ,{useState,useEffect}from 'react';
 
-function ViewResult(){
+function ViewResult() {
 
     const [datas, setDatas] = useState([]);
+
     useEffect(() => {
-    getdata();
-}, []);
+        getdata();
+    },[]);
 
-const getdata = async () => {
-    const response = await fetch(
-        "http://localhost:8080/picture"
-    );
-    const data = await response.json();
-    setDatas(data);
-};
+    const getdata = async () => {
+        const response = await fetch(
+            "http://localhost:8080/picture"
+        );
+        const data = await response.json();
+        setDatas(data);
+    };
 
-        return (
-            <div className="container">
+    return (
+        <div className="container">
+            {datas.map(data => (
                 <div className="card">
-                    <img src={datas.imageUrl} className="card-img-top" alt="..."/>
+                    <img src={data.imageUrl} className="card-img-top" alt="..."/>
                     <div className="card-body">
                         <h5 className="card-title">Result for Uploaded Image</h5>
-                        <p className="card-text">{datas.detected_text}</p>
+                        <p className="card-text">{data.detected_text}</p>
                         <button className="btn btn-danger">Delete the result</button>
                     </div>
                 </div>
-            </div>
-        );
+            ))}
+        </div>
+
+
+    );
 
 }
 
