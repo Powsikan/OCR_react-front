b3349408384e25873f155314c2c9da838b7b350f
powsokan <powsi1997@gmail.com>
2020-05-03 19:31:49 +0530

    view result api call

--- src/components/ViewResult.js
+++ src/components/ViewResult.js
@@ -1,21 +1,33 @@
-import React, {Component} from 'react';
+import React from 'react';
 
-class ViewResult extends Component {
-    render() {
+function ViewResult(){
+
+    const [datas, setDatas] = useState([]);
+    useEffect(() => {
+    getdata();
+}, []);
+
+const getdata = async () => {
+    const response = await fetch(
+        "http://localhost:8080/picture"
+    );
+    const data = await response.json();
+    setDatas(data);
+};
+
         return (
             <div className="container">
                 <div className="card">
-                    <img src="../images/upl.png" className="card-img-top" alt="..."/>
+                    <img src={datas.imageUrl} className="card-img-top" alt="..."/>
                     <div className="card-body">
                         <h5 className="card-title">Result for Uploaded Image</h5>
-                        <p className="card-text">Some quick example text to build on the card title and make up the
-                            bulk of the card's content.</p>
+                        <p className="card-text">{datas.detected_text}</p>
                         <button className="btn btn-danger">Delete the result</button>
                     </div>
                 </div>
             </div>
         );
-    }
+
 }
 
 export default ViewResult;
