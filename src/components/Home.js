1ef1c6c447c9034852479b2b5d0b1c6f968d42e0
powsokan <powsi1997@gmail.com>
2020-05-04 09:23:18 +0530

    view result api call

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,9 +3,8 @@ import React, {Component} from 'react';
 class Home extends Component {
     constructor(props) {
         super(props);
-        this.state={
-            file:null,
-            user:"powsikan"
+        this.state = {
+            file: null,
         };
         this.handleClick = this.handleClick.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -13,29 +12,28 @@ class Home extends Component {
     }
 
     handleSubmit(event) {
+
         event.preventDefault();
-        this.setState ({file: event.target.files,user: "powsikan"})
+        this.setState({file: event.target.files })
         const formData = new FormData();
-        formData.append('file', this.state.file,);
-        fetch('http://localhost:8080/picture/', {
+        formData.append('file', this.state.file);
+        console.log(formData)
+        fetch('http://localhost:8080/picture/powsikan', {
             method: 'post',
             body: formData
         }).then(res => {
-            if(res.ok) {
+            if (res.ok) {
                 console.log(res.data);
                 alert("File uploaded successfully.")
             }
         });
-
     }
 
     handleClick(event) {
-         this.refs.fileUploader.click();
-
+        this.refs.fileUploader.click();
     }
 
     render() {
-
         return (
             <div className="container">
                 <div className="jumbotron">
@@ -43,23 +41,16 @@ class Home extends Component {
                     <p className="lead">This is a simple Text recognizing application.</p>
                     <hr className="my-4"/>
                     <p>It detect the text in the Image and give back to you as text format.</p>
-
                     <form onSubmit={this.handleSubmit}>
-                        <div className="upload" name="file" onClick={this.handleClick}>
+                        <div className="upload" onClick={this.handleClick}>
                             <h5>Choose file</h5>
-                            <input type="file" name="file" ref="fileUploader"  />
+                            <input type="file" name="file" ref="fileUploader"/>
                         </div>
-
                         <button className="btn btn-primary btn-lg" type="submit">Upload Image</button>
                     </form>
-
                 </div>
-
             </div>
         );
     }
-
-
 }
-
 export default Home;
