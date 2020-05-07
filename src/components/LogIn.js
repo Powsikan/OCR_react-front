946db578311cdd023be0c7bce8f0978779861caa
powsokan <powsi1997@gmail.com>
2020-05-07 09:30:07 +0530

    login api connected

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -3,22 +3,43 @@ import {Link} from 'react-router-dom';
 import "../App.css";
 
 class LogIn extends Component {
+submitLogin(e){
+    e.preventDefault();
+
+    let user={
+        username:this.refs.email.value,
+        password:this.refs.password.value
+    }
+
+    fetch('http://localhost:8080/users/login', {
+        method: 'post',
+        body: JSON.stringify(user),
+        headers:{ 'Content-Type': 'application/json'}
+    }).then(res => {
+        if (res.ok) {
+           window.location.anchor("Home.js")
+            alert("User register successfully.")
+        }
+    });
+}
+
+
     render() {
         return (
             <div className="container">
                 <div className="cont">
                     <h2 className="heading">Login here</h2>
-                    <form>
+                    <form onSubmit={this.submitLogin.bind(this)}>
                         <div className="form-group">
                             <label htmlFor="exampleInputEmail1">Email address</label>
-                            <input type="email" className="form-control" id="exampleInputEmail1"
-                                   aria-describedby="emailHelp" placeholder="example@abc.com"/>
+                            <input ref="email" type="email" className="form-control" id="exampleInputEmail1"
+                                   aria-describedby="emailHelp" placeholder="example@abc.com" />
                         </div>
                         <div className="form-group">
                             <label htmlFor="exampleInputPassword1">Password</label>
-                            <input type="password" className="form-control" id="exampleInputPassword1"/>
+                            <input ref="password" type="password" className="form-control" id="exampleInputPassword1" />
                         </div>
-                     <Link to={'/'}> <button type="submit" className="btn btn-primary">LogIn</button></Link>
+                     <button type="submit" className="btn btn-primary">LogIn</button>
                     </form>
                 </div>
             </div>
