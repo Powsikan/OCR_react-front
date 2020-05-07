f397ba5097d5bb114d27d5ced42cf60c5f39d593
powsokan <powsi1997@gmail.com>
2020-05-07 08:22:42 +0530

    upload api connected

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 import "../App.css";
-import Home from "./Home";
 
 class SignUp extends Component {
     constructor(props) {
