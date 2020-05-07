caacacbce9f18691e966bd4f011ad6e14eec838c
powsokan <powsi1997@gmail.com>
2020-05-07 21:18:45 +0530

    login api connected

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
-import NavBar from "./components/NavBar";
+import NavBar from "./components/nav/NavBar";
 import Home from "./components/Home";
 import LogIn from "./components/LogIn";
 import SignUp from "./components/SignUp";
@@ -18,7 +18,7 @@ function App() {
                     <hr/>
                     <Switch>
                         <Route exact path='/' component={LogIn}/>
-                        <Route path='/home' component={Home}/>
+              nav          <Route path='/home' component={Home}/>
                         <Route path='/signup' component={SignUp}/>
                         <Route path='/viewresult' component={ViewResult}/>
                     </Switch>
---
+++ src/components/nav/NavBarHome.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+
+class NavBar extends Component {
+    render() {
+        return (
+            <div>
+                <nav className="navbar navbar-expand-lg navbar-dark bg-secondary rounded">
+                    <a className="navbar-brand" href="/">OCR</a>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav  mr-auto">
+                            <li><Link to={'/home' } className="nav-link " >Home</Link></li>
+                            <li><Link to={'/viewresult'} className="  nav-link" >View Result</Link></li>
+                        </ul>
+                        <ul className="nav navbar-nav  ml-auto">
+                            <li><Link to={'/logout'} className=" nav-link" > LogOut </Link></li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        );
+    }
+}
+
+export default NavBar;
+
